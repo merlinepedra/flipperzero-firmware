2a56d228bc3f1a0e526f22a8b985e1dcc644385d
DrZlo13 <who.just.the.doctor@gmail.com>
2021-12-21 16:20:08 +1000

    elf-loader: compile-time abi validation

--- applications/elf-loader/elf-cpp/elf-hashtable.cpp
+++ applications/elf-loader/elf-cpp/elf-hashtable.cpp
@@ -9,9 +9,9 @@
 
 #define TAG "elf-hashtable"
 
-#define API_METHOD(x)                                    \
-    sym_entry {                                          \
-        .hash = elf_gnu_hash(#x), .address = (uint32_t)x \
+#define API_METHOD(x, ret_type, args_type)                                                     \
+    sym_entry {                                                                                \
+        .hash = elf_gnu_hash(#x), .address = (uint32_t)(static_cast<ret_type(*) args_type>(x)) \
     }
 
 struct sym_entry {
@@ -29,44 +29,62 @@ static constexpr uint32_t elf_gnu_hash(const char* s) {
     return h;
 }
 
-static constexpr auto elf_addr_table = sort(create_array_t<sym_entry>(
-    API_METHOD(acquire_mutex),
-    API_METHOD(canvas_draw_frame),
-    API_METHOD(canvas_draw_rframe),
-    API_METHOD(canvas_draw_box),
-    API_METHOD(canvas_set_color),
-    API_METHOD(canvas_set_font),
-    API_METHOD(canvas_draw_str),
-    API_METHOD(snprintf),
-    API_METHOD(canvas_draw_str_aligned),
-    API_METHOD(release_mutex),
-    API_METHOD(osMessageQueuePut),
-    API_METHOD(memset),
-    API_METHOD(rand),
-    API_METHOD(memmove),
-    API_METHOD(srand),
-    API_METHOD(osMessageQueueNew),
-    API_METHOD(furi_alloc),
-    API_METHOD(init_mutex),
-    API_METHOD(furi_log_print),
-    API_METHOD(free),
-    API_METHOD(view_port_alloc),
-    API_METHOD(view_port_draw_callback_set),
-    API_METHOD(view_port_input_callback_set),
-    API_METHOD(osTimerNew),
-    API_METHOD(osKernelGetTickFreq),
-    API_METHOD(osTimerStart),
-    API_METHOD(furi_record_open),
-    API_METHOD(gui_add_view_port),
-    API_METHOD(osMessageQueueGet),
-    API_METHOD(view_port_update),
-    API_METHOD(osTimerDelete),
-    API_METHOD(view_port_enabled_set),
-    API_METHOD(gui_remove_view_port),
-    API_METHOD(furi_record_close),
-    API_METHOD(view_port_free),
-    API_METHOD(osMessageQueueDelete),
-    API_METHOD(delete_mutex)));
+/**
+ * 
+ */
+static const constexpr auto elf_api_table = sort(create_array_t<sym_entry>(
+    API_METHOD(init_mutex, bool, (ValueMutex*, void*, size_t)),
+    API_METHOD(acquire_mutex, void*, (ValueMutex*, uint32_t)),
+    API_METHOD(release_mutex, bool, (ValueMutex*, const void*)),
+    API_METHOD(delete_mutex, bool, (ValueMutex*)),
+
+    API_METHOD(canvas_draw_frame, void, (Canvas*, uint8_t, uint8_t, uint8_t, uint8_t)),
+    API_METHOD(canvas_draw_rframe, void, (Canvas*, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t)),
+    API_METHOD(canvas_draw_box, void, (Canvas*, uint8_t, uint8_t, uint8_t, uint8_t)),
+    API_METHOD(canvas_set_color, void, (Canvas*, Color)),
+    API_METHOD(canvas_set_font, void, (Canvas*, Font)),
+    API_METHOD(canvas_draw_str, void, (Canvas*, uint8_t, uint8_t, const char*)),
+    API_METHOD(
+        canvas_draw_str_aligned,
+        void,
+        (Canvas*, uint8_t, uint8_t, Align, Align, const char*)),
+
+    API_METHOD(view_port_alloc, ViewPort*, (void)),
+    API_METHOD(view_port_draw_callback_set, void, (ViewPort*, ViewPortDrawCallback, void*)),
+    API_METHOD(view_port_input_callback_set, void, (ViewPort*, ViewPortInputCallback, void*)),
+
+    API_METHOD(rand, int, (void)),
+    API_METHOD(srand, void, (unsigned)),
+    API_METHOD(snprintf, int, (char*, size_t, const char*, ...)),
+    API_METHOD(memset, void*, (void*, int, size_t)),
+    API_METHOD(memmove, void*, (void*, const void*, size_t)),
+    API_METHOD(free, void, (void*)),
+
+    API_METHOD(osTimerNew, osTimerId_t, (osTimerFunc_t, osTimerType_t, void*, const osTimerAttr_t*)),
+    API_METHOD(osKernelGetTickFreq, uint32_t, (void)),
+    API_METHOD(osTimerDelete, osStatus_t, (osTimerId_t)),
+    API_METHOD(osTimerStart, osStatus_t, (osTimerId_t, uint32_t)),
+    API_METHOD(
+        osMessageQueueNew,
+        osMessageQueueId_t,
+        (uint32_t, uint32_t, const osMessageQueueAttr_t*)),
+    API_METHOD(
+        osMessageQueueGet,
+        osStatus_t,
+        (osMessageQueueId_t, void*, uint8_t*, uint32_t timout)),
+    API_METHOD(osMessageQueuePut, osStatus_t, (osMessageQueueId_t, const void*, uint8_t, uint32_t)),
+    API_METHOD(osMessageQueueDelete, osStatus_t, (osMessageQueueId_t)),
+
+    API_METHOD(furi_alloc, void*, (size_t)),
+    API_METHOD(furi_log_print, void, (FuriLogLevel, const char*, ...)),
+    API_METHOD(furi_record_open, void*, (const char*)),
+    API_METHOD(furi_record_close, void, (const char* name)),
+
+    API_METHOD(gui_add_view_port, void, (Gui*, ViewPort*, GuiLayer)),
+    API_METHOD(gui_remove_view_port, void, (Gui*, ViewPort*)),
+    API_METHOD(view_port_update, void, (ViewPort*)),
+    API_METHOD(view_port_enabled_set, void, (ViewPort*, bool)),
+    API_METHOD(view_port_free, void, (ViewPort*))));
 
 extern "C" bool elf_resolve_from_hashtable(const char* name, Elf32_Addr* address) {
     bool result = false;
@@ -74,8 +92,8 @@ extern "C" bool elf_resolve_from_hashtable(const char* name, Elf32_Addr* address
 
     sym_entry key = {.hash = gnu_sym_hash};
 
-    auto find_res = std::lower_bound(elf_addr_table.cbegin(), elf_addr_table.cend(), key);
-    if((find_res == elf_addr_table.cend() || (find_res->hash != gnu_sym_hash))) {
+    auto find_res = std::lower_bound(elf_api_table.cbegin(), elf_api_table.cend(), key);
+    if((find_res == elf_api_table.cend() || (find_res->hash != gnu_sym_hash))) {
         FURI_LOG_W(TAG, "Cant find symbol '%s' (hash %x)!", name, gnu_sym_hash);
         result = false;
     } else {
